3e9f006c53e5514f8a777c9a9ac6dfe45bd34688
张悦 <1647160039@qq.com>
2018-06-21 09:32:07 +0800

    ddddda

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  c
+//
+//  Created by zhangyue on 18/6/21.
+//  Copyright © 2018年 zhangyue. All rights reserved.
+//
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    int sum;
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
