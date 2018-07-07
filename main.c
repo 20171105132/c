0b3ebe7d62433eea40c91937a4be5338b85fd38d
张悦 <1647160039@qq.com>
2018-07-07 11:59:30 +0800

    增加了平均值
    加了平均值和循环输出其他行

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ struct  judge
 int main()
 {
     FILE *a;
+    FILE *b;
     char numbers[10];
     char name[10];
     char sex[10];
@@ -38,4 +39,56 @@ int main()
         }
         fclose(a);
     }
+    j=i;
+    int max[100],min[100];
+    for(i=0;i<j;i++)
+    {
+        max[i]=min[i]=p[i].judge1;
+    }
+    j=i;
+    
+    for(i=0;i<j;i++)
+    {
+        if(p[i].judge2>max[i])
+            max[i]=p[i].judge2;
+        if(p[i].judge3>max[i])
+            max[i]=p[i].judge3;
+        if(p[i].judge4>max[i])
+            max[i]=p[i].judge4;
+        if(p[i].judge5>max[i])
+            max[i]=p[i].judge5;
+    }
+    j=i;
+    
+    for(i=0;i<j;i++)
+    {
+        if(p[i].judge2<min[i])
+            min[i]=p[i].judge2;
+        if(p[i].judge3<min[i])
+            min[i]=p[i].judge3;
+        if(p[i].judge4<min[i])
+            min[i]=p[i].judge4;
+        if(p[i].judge5<min[i])
+            min[i]=p[i].judge5;
+    }
+    j=i;
+    
+    for(i=0;i<j;i++)
+    {
+        p[i].score=(p[i].judge1+p[i].judge2+p[i].judge3+p[i].judge4+p[i].judge5-max[i]-min[i])/3;
+    }
+    j=i;
+    i=0;
+    b=fopen("/Users/S20171105132/Desktop/studentdate2.csv","s");
+    
+    while(i<j)
+    {
+        fprintf(b,"%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d,%d",numbers,name,sex,dateofbirt,class1,phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
+        i++;
+    }
+    fclose(b);
+    
+    return 0;
 }
+
+
