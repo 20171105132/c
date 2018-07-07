f0c5f3072ce159348a4275bc1b544af45c5c06b5
张悦 <1647160039@qq.com>
2018-07-07 11:39:41 +0800

    数据的循环输出
    用了结构体，但是不能输出

--- main.c
+++ main.c
@@ -1,18 +1,41 @@
-//
-//  main.c
-//  c
-//
-//  Created by zhangyue on 18/6/21.
-//  Copyright © 2018年 zhangyue. All rights reserved.
-//
-
 #include<stdio.h>
+struct  judge
+{
+    int judge1;
+    int judge2;
+    int judge3;
+    int judge4;
+    int judge5;
+    int score;
+};
 int main()
 {
-    int a,b;
-    int sum;
-    scanf("%d %d",&a,&b);
-    sum=a+b;
-    printf("%d",sum);
-    return 0;
+    FILE *a;
+    char numbers[10];
+    char name[10];
+    char sex[10];
+    char dateofbirt[15];
+    char class1[15];
+    char phoneNo[15];
+    struct judge p[100];
+    int i=0,j;
+    if ((a=fopen("/Users/s20171105132/Desktop/studentdata.csv","r"))==0)
+    {
+        printf("文件不存在\n");
+    }
+    else
+    {
+        while(!feof(a))
+        {
+            fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%d ,%d ,%d ,%d ,%d ",numbers,name,sex,dateofbirt,class1,phoneNo,&p[i].judge1,&p[i].judge2,&p[i].judge3,&p[i].judge4,&p[i].judge5);
+            printf("%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d",numbers,name,sex,dateofbirt,class1,phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5);
+            i++;
+        }
+        j=i;
+        for(i=0;i<j;i++)
+        {
+            printf("%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d",numbers,name,sex,dateofbirt,class1,phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5);
+        }
+        fclose(a);
+    }
 }
