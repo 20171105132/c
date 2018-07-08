e5c3ecd9b5f005835d789711407710e1fdd716bf
张悦 <1647160039@qq.com>
2018-07-08 18:37:34 +0800

    无法输出
    文件名不存在

--- main.c
+++ main.c
@@ -28,62 +28,71 @@ int main()
 {
     FILE *a;
     FILE *b;
-    int  n,i=0;
-    struct student p[100];
-    if ((a=fopen("/Users/s20171105132/Desktop/studentdate.csv","r"))==0)
+    int  n,i=0,t=0;
+    scanf("%d",&n);
+    struct student p[n];
+    struct student *head,*h;
+    head=&p[t];
+    p[t].next=&p[t+1];
+    h=head;
+    b=fopen("/Users/s20171105132/Desktop/studentdata2.csv","s");
+    if ((a=fopen("/Users/s20171105132/Desktop/studentdata","r"))==0)
     {
         printf("文件不存在\n");
     }
     else
     {
-        fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ",p[i].Numbers,p[i].name,p[i].sex,p[i].Dateofbirt,p[i].class1,p[i].phoneNo,p[i].Judge1,p[i].Judge2,p[i].Judge3,p[i].Judge4,p[i].Judge5);
-        while(!feof(a))
-           {
-            printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5);
-               i++;
-           }
-        
+        fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ",&p[i].Numbers,&p[i].name,&p[i].sex,&p[i].Dateofbirt,&p[i].class1,&p[i].phoneNo,&p[i].Judge1,&p[i].Judge2,&p[i].Judge3,&p[i].Judge4,&p[i].Judge5);
+        fprintf(b,"numbers,name,sex,dateofbirth,class,phoneNo,Judge1,Judge2,Judge3,Judge4,Judge5,score\n");
+
+        h=h->next;
+        for(i=0;i<n;i++)
+        {
+            while(h!=0)
+            {
+               fscanf(a,"%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d",&p[i].numbers,p[i].name,p[i].sex,&p[i].dateofbirt,p[i].class1,p[i].phoneNo,&p[i].judge1,&p[i].judge2,&p[i].judge3,&p[i].judge4,&p[i].judge5);
+                h=h->next;
+                break;
+            }
+        }
     }
-    n=i;
     int max[50],min[50];
     for(i=0;i<n;i++)
     {
-         max[i]=min[i]=p[i].judge1;
+        max[i]=min[i]=p[i].judge1;
     }
     for(i=0;i<n;i++)
     {
-    if(p[i].judge2>max[i])
-      max[i]=p[i].judge2;
-    if(p[i].judge3>max[i])
-      max[i]=p[i].judge3;
-    if(p[i].judge4>max[i])
-      max[i]=p[i].judge4;
-    if(p[i].judge5>max[i])
-      max[i]=p[i].judge5;
-    }
-    for(i=0;i<n;i++)
-   {
-    if(p[i].judge2<min[i])
-      min[i]=p[i].judge2;
-    if(p[i].judge3<min[i])
-      min[i]=p[i].judge3;
-    if(p[i].judge4<min[i])
-      min[i]=p[i].judge4;
-    if(p[i].judge5<min[i])
-      min[i]=p[i].judge5;
+        if(p[i].judge2>max[i])
+            max[i]=p[i].judge2;
+        if(p[i].judge3>max[i])
+            max[i]=p[i].judge3;
+        if(p[i].judge4>max[i])
+            max[i]=p[i].judge4;
+        if(p[i].judge5>max[i])
+            max[i]=p[i].judge5;
     }
     for(i=0;i<n;i++)
     {
-    p[i].score=(p[i].judge1+p[i].judge2+p[i].judge3+p[i].judge4+p[i].judge5-max[i]-min[i])/3;
+        if(p[i].judge2<min[i])
+            min[i]=p[i].judge2;
+        if(p[i].judge3<min[i])
+            min[i]=p[i].judge3;
+        if(p[i].judge4<min[i])
+            min[i]=p[i].judge4;
+        if(p[i].judge5<min[i])
+            min[i]=p[i].judge5;
     }
-    b=fopen("/Users/S20171105132/Desktop/studentdate2.csv","s");
-    fprintf(b,"numbers,name,sex,dateofbirth,class,phoneNo,judge1,judge2,judge3,judge4,judge5,score\n");
+    for(i=0;i<n;i++)
+    {
+        p[i].score=(p[i].judge1+p[i].judge2+p[i].judge3+p[i].judge4+p[i].judge5-max[i]-min[i])/3;
+    }
     i=0;
     while(i<n)
     {
-        printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
-        fprintf(b,"%d/%s/%s/%d/%s/%s/%d/%d/%d/%d/%d/%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
+        fprintf(b,"%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
+        printf("%d/%s/%s/%d/%s/%s/%d/%d/%d/%d/%d/%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
         i++;
     }
-   return 0;
+    return 0;
 }
