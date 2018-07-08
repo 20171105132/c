d84957b3a80c6ed8b9f29bb325862574f25f3a53
张悦 <1647160039@qq.com>
2018-07-08 11:51:57 +0800

    无法输出
    执行完毕后文件不存在，又定义了两个数组。

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 struct student
 {
-    char numbers[10];
     char name[10];
     char sex[10];
-    char dateofbirt[15];
     char class1[15];
     char phoneNo[15];
+    char Judge1[15];
+    char Judge2[15];
+    char Judge3[15];
+    char Judge4[15];
+    char Judge5[15];
+    char Numbers[15];
+    char Dateofbirt[15];
+    int numbers;
+    int dateofbirt;
     int judge1;
     int judge2;
     int judge3;
@@ -19,36 +28,23 @@ int main()
 {
     FILE *a;
     FILE *b;
-    char numbers[10];
-    char name[10];
-    char sex[10];
-    char dateofbirt[15];
-    char class1[15];
-    char phoneNo[15];
-    int  t=0,n,i;
-    scanf("%d",n);
-    struct student *head,*h,p[n];
-    head=&p[t];
-    p[t].next=&p[t+1];
-    h=head;
-    if ((a=fopen("/Users/s20171105132/Desktop/studentdata.csv","r"))==0)
+    int  n,i=0;
+    struct student p[100];
+    if ((a=fopen("/Users/s20171105132/Desktop/studentdate.csv","r"))==0)
     {
         printf("文件不存在\n");
     }
     else
     {
-        fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ",&p[i].numbers,&p[i].name,&p[i].sex,&p[i].dateofbirt,&p[i].class1,&p[i].phoneNo,&p[i].judge1,&p[i].judge2,&p[i].judge3,&p[i].judge4,&p[i].judge5);
-        printf("%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5);
-        for(i=0;i<n;i++)
-        {
-             while(h!=0)
+        fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ",p[i].Numbers,p[i].name,p[i].sex,p[i].Dateofbirt,p[i].class1,p[i].phoneNo,p[i].Judge1,p[i].Judge2,p[i].Judge3,p[i].Judge4,p[i].Judge5);
+        while(!feof(a))
            {
-            printf("%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5);
-            h=h->next;
-               break;
+            printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5);
+               i++;
            }
-        }
+        
     }
+    n=i;
     int max[50],min[50];
     for(i=0;i<n;i++)
     {
@@ -82,10 +78,12 @@ int main()
     }
     b=fopen("/Users/S20171105132/Desktop/studentdate2.csv","s");
     fprintf(b,"numbers,name,sex,dateofbirth,class,phoneNo,judge1,judge2,judge3,judge4,judge5,score\n");
+    i=0;
     while(i<n)
     {
-    fprintf(b,"%s,%s,%s,%s,%s,%s,%d,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
-       i++;
+        printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
+        fprintf(b,"%d/%s/%s/%d/%s/%s/%d/%d/%d/%d/%d/%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
+        i++;
     }
    return 0;
 }
