29375733b7421f6a28049952c5e23e1a42c4c31c
张悦 <40412818+20171105132@users.noreply.github.com>
2018-07-09 17:04:55 +0800

    正常输出
    基本实现了功能

--- ac.cpp
+++ ac.cpp
@@ -23,7 +23,7 @@ int main()
     int  n,i=0;
     char header[100];
     struct student p[100];
-    a=fopen("/Users/s20171105115/Desktop/studentdata.csv","r");
+    a=fopen("studentdata.csv","r");
     if (a==NULL)
     {
         printf("文件不存在\n");
@@ -72,7 +72,7 @@ int main()
     {
         p[i].score=(p[i].judge1+p[i].judge2+p[i].judge3+p[i].judge4+p[i].judge5-max[i]-min[i])/3;
     }
-    b=fopen("/Users/s20171105115/Desktop/studentdata2.csv","s");
+    b=fopen("studentdata2.csv","w");
     if(b==0)
     {
         printf("文件不存在");
@@ -80,13 +80,13 @@ int main()
     }
     else
     {
-        fprintf(b,"%s,aver\n",header);
-        printf("%s,average\n",header);
+        fprintf(b,"%s,score\n",header);
+        printf("%s,score\n",header);
         i=0;
         while(i<n)
        {
-           fprintf(b,"%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d,%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
-           printf("%d/%s/%s/%d/%s/%s/%d/%d/%d/%d/%d/%d",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
+           fprintf(b,"%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d,%d\n",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
+           printf("%d,%s,%s,%d,%s,%s,%d,%d,%d,%d,%d,%d\n",p[i].numbers,p[i].name,p[i].sex,p[i].dateofbirt,p[i].class1,p[i].phoneNo,p[i].judge1,p[i].judge2,p[i].judge3,p[i].judge4,p[i].judge5,p[i].score);
            i++;
        }
     }
